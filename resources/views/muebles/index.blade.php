a500a3b69d8de84fdbec79212bdf3b6dec492d72
daw <juanmiguel.valle@iesdonana.org>
2025-03-17 21:20:05 +0100

    aqui quedo

---
+++ resources/views/muebles/index.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Muebles
+        </h2>
+    </x-slot>
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex gap-x-20">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+
+                            @livewire('buscador-alumnos')
+                        </div>
+                        {{-- <div class="mt-6 text-center">
+                            <a href="{{ route('alumnos.create') }}"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                Crear un nuevo alumno
+                            </a>
+                        </div> --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
